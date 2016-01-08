ff6222cda50e707a5525e018ea20a232c00f4d37
dannyvassallo <danielvassallo87@gmail.com>
2016-01-08 15:15:06 -0500

    trying my detector

--- source/javascripts/_custom.js
+++ source/javascripts/_custom.js
@@ -1,36 +1,36 @@
 var currentRulesLink = 'http://www.radiodisneyapp.com/rules-files/demilovatorules.pdf'
 
-// //Detection Script//
-//   var iPadAgent = navigator.userAgent.match(/iPad/i) != null;
-//   var iPodAgent = navigator.userAgent.match(/iPhone/i) != null;
-//   var AndroidAgent = navigator.userAgent.match(/Android/i) != null;
-//   var webOSAgent = navigator.userAgent.match(/webOS/i) != null;
-// //iOS//
-//   if(iPadAgent || iPodAgent){
-//     ga('send', 'event', 'Detection', 'Device', 'iOS');
-//     var windowLocation = window.location.pathname;
-//     if(windowLocation == '/'){
-//       window.location.replace("https://itunes.apple.com/us/app/radio-disney-junior/id1050711248?mt=8");
-//     } else if(windowLocation == '/rules'){
-//       window.location.replace(currentRulesLink);
-//     }
-//   }
-// //Android//
-//   else if(AndroidAgent){
-//     ga('send', 'event', 'Detection', 'Device', 'Android');
-//     var windowLocation = window.location.pathname;
-//     if(windowLocation == '/'){
-//       // window.location.replace("http://bit.ly/1f7t6Zj");
-//     } else if(windowLocation == '/rules'){
-//       window.location.replace(currentRulesLink);
-//     }
-//   }
-// //Other//
-//   else{
-//     ga('send', 'event', 'Detection', 'Device', 'Not an iPhone or Android');
-//     var windowLocation = window.location.pathname;
-//     // console.log(windowLocation);
-//   }
+//Detection Script//
+  var iPadAgent = navigator.userAgent.match(/iPad/i) != null;
+  var iPodAgent = navigator.userAgent.match(/iPhone/i) != null;
+  var AndroidAgent = navigator.userAgent.match(/Android/i) != null;
+  var webOSAgent = navigator.userAgent.match(/webOS/i) != null;
+//iOS//
+  if(iPadAgent || iPodAgent){
+    ga('send', 'event', 'Detection', 'Device', 'iOS');
+    var windowLocation = window.location.pathname;
+    if(windowLocation == '/'){
+      window.location.replace("http://itunes.com/apps/radiodisneyjunior");
+    } else if(windowLocation == '/rules'){
+      window.location.replace(currentRulesLink);
+    }
+  }
+//Android//
+  else if(AndroidAgent){
+    ga('send', 'event', 'Detection', 'Device', 'Android');
+    var windowLocation = window.location.pathname;
+    if(windowLocation == '/'){
+      window.location.replace("https://play.google.com/store/apps/details?id=com.disney.radiodisneyjunior_goo&hl=en");
+    } else if(windowLocation == '/rules'){
+      window.location.replace(currentRulesLink);
+    }
+  }
+//Other//
+  else{
+    ga('send', 'event', 'Detection', 'Device', 'Not an iPhone or Android');
+    var windowLocation = window.location.pathname;
+    // console.log(windowLocation);
+  }
 
 
 // Clicks
